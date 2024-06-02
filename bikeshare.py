b1995064cd6aab07c3eb6ca8b643b2d563ea5a03
khanha1111 <khanhht.qt@gmail.com>
2024-06-02 09:42:37 +0700

    add 1 todo commend code

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 #Creating a dictionary containing the data sources for the three cities
+#Data city
 CITY_DATA = { 'chicago': 'data/chicago.csv', 'Chicago': 'data/chicago.csv',
              'New York City': 'data/new_york_city.csv', 'New york city': 'data/new_york_city.csv',
               'new york city': 'data/new_york_city.csv', 'washington': 'data/washington.csv',
